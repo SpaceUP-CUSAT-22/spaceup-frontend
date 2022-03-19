8ae870329fcbd8a0a4e705907cc240f96d6427fb
Rohith C Dinesan <rohithmaddz7@gmail.com>
2022-03-19 19:14:31 +0530

    minor fixes

--- src/Components/Milestone/Milestone.js
+++ src/Components/Milestone/Milestone.js
@@ -2,18 +2,18 @@ import "./Milestone.css";
 
 const content = [
     {
-        image: "",
+        image: "https://picsum.photos/id/237/200/300",
         title: "SPACEUP 2020",
         desc: "IRES SEDS CUSAT conducted the third SPACE-UP Unconference ever in India. It was an accumulation of various collaborative events, seminars, forums and to provide the participants what is happening in the industry and to ask questions and learn from them. An opportunity to interact with some of the greats in the field."
     },
 
     {
-        image: "",
+        image: "https://picsum.photos/id/237/200/300",
         title: "DRONE MAKING WORKSHOP",
         desc: "The Drone Making Workshop had a goal for participants to learn the basics of drone making with  the limited resources provided, to build and fly their drone with more confidence in a safe way. Withexperts there to help them learn and create."
     },
     {
-        image: "",
+        image: "https://picsum.photos/id/237/200/300",
         title: "CUSAT WORKSHOP",
         desc: "The workshop aimed at designing and providing the knowledge of space science, satellites, electronics, and embedded system from basic to advance levels. Basic learning was provided to the participants on the making of miniature satellites."
     }
@@ -26,11 +26,13 @@ const Milestone = () => {
                 <div className="milestone-heading">
                     <h1>Milestone</h1>
                 </div>
+                <div className="milestone-body">
                 {
                     content.map((item) => (
                         <Card image={item.image} title={item.title} desc={item.desc}/>
                     ))
                 }
+                </div>
             </div>
         </>
 
