78ef369da7ca77cfbe2b643fd55a849b2e42308f
Rohith C Dinesan <rohithmaddz7@gmail.com>
2022-03-19 15:22:50 +0530

    Major change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home";
-import About from "./Components/About/About";
-import Gallery from "./Components/Gallery/Gallery";
+import Main from "./Main.js";
 import bgimage from "./Assets/home-bg.jpg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+/* --------------------------------------------------
+  Import Components in the Main.js file 
+  ---------------------------------------------------*/
 
 function App() {
   return (
@@ -12,10 +15,14 @@ function App() {
       <div className="bg-home">
         <img src={bgimage} alt="" />
       </div>
-      <Navbar />
-      <Home />
-      <About />
-      <Gallery />
+      <Router>
+        <Switch>
+            <Route exact path="/" component={Main} />
+            {/* <Route path="/login" component={Login} */}
+            {/* <Route component={Error}/> */}
+        </Switch>
+      </Router>
+
     </>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Gallery from "./Components/Gallery/Gallery";
+
+const Main = () => {
+    return (
+        <>
+            <Navbar />
+            <Home />
+            <About />
+            <Gallery />
+        </>
+    );
+}
+
+export default Main;
