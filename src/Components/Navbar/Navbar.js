a4e0c217af6afd1369916aa5b7af9c6e7356c26a
Rohith C Dinesan <rohithmaddz7@gmail.com>
2022-03-19 12:55:19 +0530

    Updated Home.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,10 @@
 import "./Navbar.css";
-import {useState} from 'react';
-import logo from "../../Assets/logo.png";
+import { useState } from 'react';
+import logo from "../../Assets/space_up-logo.png";
 
 const Navbar = () => {
   const [drop, setDrop] = useState(true);
-
-  
+  const navItems = ["Home", "About", "SSS", "Events", "Milestone", "Team", "Speakers", "Gallery"];
 
   return (
     <>
@@ -13,40 +12,19 @@ const Navbar = () => {
         <div className="container-fluid container-xxl d-flex align-items-center">
 
           <div id="logo" className="me-auto">
-            <a href="index.html" className="scrollto"><img src={logo} alt=""/></a>
+            <a href="index.html" className="scrollto"><img src={logo} alt="" /></a>
           </div>
 
           <nav id="navbar" className="navbar order-last order-lg-0">
             <ul>
-              <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
-              <li><a className="nav-link scrollto" href="#about">About</a></li>
-              <li><a className="nav-link scrollto" href="#speakers">Speakers</a></li>
-              <li><a className="nav-link scrollto" href="#schedule">Schedule</a></li>
-              <li><a className="nav-link scrollto" href="#venue">Venue</a></li>
-              <li><a className="nav-link scrollto" href="#hotels">Hotels</a></li>
-              <li><a className="nav-link scrollto" href="#gallery">Gallery</a></li>
-              <li><a className="nav-link scrollto" href="#supporters">Sponsors</a></li>
-              <li className="dropdown" id="event"><a href="#"><span>Event</span> <i className="bi bi-chevron-down"></i></a>
-              
-          <ul>
-         
-            <li><a href="#">Drop Down 1</a></li>
-            <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-              <ul>
-                <li><a href="#">Deep Drop Down 1</a></li>
-                <li><a href="#">Deep Drop Down 2</a></li>
-                <li><a href="#">Deep Drop Down 3</a></li>
-                <li><a href="#">Deep Drop Down 4</a></li>
-                <li><a href="#">Deep Drop Down 5</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Drop Down 2</a></li>
-            <li><a href="#">Drop Down 3</a></li>
-            <li><a href="#">Drop Down 4</a></li>
-          </ul>
-        </li> 
-              <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+              {navItems.map((item, index) => (
+                <li className="dropdown">
+                  <a className="nav-link scrollto" href="">{item}</a>
+                  <DropDown index={index}/>
+                </li>
+              ))}
             </ul>
+
             <i className="bi bi-list mobile-nav-toggle"></i>
           </nav>
           <a className="buy-tickets scrollto" href="#buy-tickets">Buy Tickets</a>
@@ -59,10 +37,22 @@ const Navbar = () => {
 
 export default Navbar;
 
-const DropDown = () => {
-  return (
+const DropDown = ({index}) => {
+  const dropItems = ["drop1", "drop2", "drop3", "drop4"];
+  
+  if(index === 3) {
+    return (
     <>
-    hellooo
+      <ul>
+        {dropItems.map((item) => (
+          <li><a href="#">{item}</a></li>
+        ))}
+      </ul>
+      
     </>
-  );
+  ); 
+ }
+ else {
+   return null;
+ }
 }
