9ecdb862707cacf5640b8c0715e36a946444ffaf
Revanth <revanth123.rp40@gmail.com>
2022-03-19 09:43:57 +0530

    about page

---
+++ src/Components/About/About.js
@@ -0,0 +1,33 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <div className="parent">
+        <div className="container" data-aos="fade-up">
+          <div className="row">
+            <div className="col-lg-6">
+              <h2>About The Event</h2>
+              <p>
+                Sed nam ut dolor qui repellendus iusto odit. Possimus inventore
+                eveniet accusamus error amet eius aut accusantium et. Non odit
+                consequatur repudiandae sequi ea odio molestiae. Enim possimus
+                sunt inventore in est ut optio sequi unde.
+              </p>
+            </div>
+            <div className="col-lg-3">
+              <h3>Where</h3>
+              <p>Downtown Conference Center, New York</p>
+            </div>
+            <div className="col-lg-3">
+              <h3>When</h3>
+              <p>Monday to Wednesday 10-12 December</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
