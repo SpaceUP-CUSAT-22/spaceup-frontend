0ed03c2ee4c3bdfdd68e2a745c74f8465efcbb30
Rohith C Dinesan <rohithmaddz7@gmail.com>
2022-03-19 18:04:07 +0530

    latest update

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Gallery from "./Components/Gallery/Gallery";
-
+import Milestone from "./Components/Milestone/Milestone";
 
 const Main = () => {
     return (
@@ -11,7 +11,7 @@ const Main = () => {
             <Home />
             <About />
             <Gallery />
-            
+            <Milestone/>
         </>
     );
 }
