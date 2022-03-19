365a2be14b409f51a166f49cc64ee76b73a431dd
Revanth <revanth123.rp40@gmail.com>
2022-03-19 10:28:59 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
