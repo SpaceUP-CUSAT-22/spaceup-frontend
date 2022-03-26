e57ea73eb58f8fc98f48cf8f00f63a0dbc31024d
sharun <sharunpublic@gmai.com>
2022-03-27 03:27:13 +0530

    Completed homepage without Navbar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,24 +1,27 @@
 import "./Home.css";
 import Typewriter from "typewriter-effect";
 import Button from "../Elements/Button";
+import Pulse from "react-reveal/Pulse";
 
 const Home = () => {
 
     return (
         <>
             <section id="home">
-                <div className="home-container" data-aos="zoom-in" data-aos-delay="100">
+                <div className="home-container">
                     <h1 className="typing mb-4 pb-0">
                         <Typewriter
                             onInit={(typewriter) => {
                                 typewriter
-                                .typeString("SPACE-UP CUSAT<br/>2022")
-                                .start();
+                                    .typeString("SPACE-UP CUSAT 2022")
+                                    .start();
                             }}
                         />
-                        </h1>
-                    <p className="mb-0 pb-0">Pre-event on 3rd April 2022, Venue Seminar complex</p>
-                    <Button name="Register Now" route="/login"/>
+                    </h1>
+                    <p>Pre-event on 3rd April 2022, Venue Seminar complex</p>
+                    <Pulse forever={true}>
+                        <Button name="Register Now" route="/login" />
+                    </Pulse>
                 </div>
             </section>
         </>
--- src/Components/Speakers/Speakers.js
+++ src/Components/Speakers/Speakers.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Speakers.css";
 import "reactjs-popup/dist/index.css";
 import Popup from "./Popup";
+import Star from '../Stars/Star';
 
 export default function Speakers() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,135 +13,136 @@ export default function Speakers() {
 
   return (
     <section id="speakers">
-      <div className="speakers-head">
-        <h2>SPEAKERS</h2>
-      </div>
-
-      <div className="speaker-main">
-        <div className="sub">
-          <div className="speaker-img">
-            <img
-              src={
-                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHw%3D&w=1000&q=80"
-              }
-              alt=""
-            />
-          </div>
-          <div className="speaker-content">
-            <h2>Lorem, ipsum.</h2>
-            <p style={{ margin: "0", padding: "0" }}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
-              fugit eveniet, ullam deserunt inventore omnis!
-            </p>
-            <p style={{ color: "#f36e2d" }} onClick={togglePopup} className="more">
-              Read more...
-            </p> 
-            {isOpen && (
-              <Popup
-                content={
-                  <>
-                    <b>Info About speaker</b>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </p>
-                  </>
+      <Star />
+      <div className="container">
+        <div className="speakers-head">
+          <h2 className="section-header">SPEAKERS</h2>
+        </div>
+        <div className="speaker-main">
+          <div className="sub">
+            <div className="speaker-img">
+              <img
+                src={
+                  "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHw%3D&w=1000&q=80"
                 }
-                handleClose={togglePopup}
+                alt=""
               />
-            )}
+            </div>
+            <div className="speaker-content">
+              <h2>Lorem, ipsum.</h2>
+              <p className="speaker-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+                fugit eveniet, ullam deserunt inventore omnis!
+              </p>
+              <p style={{ color: "#f36e2d" }} onClick={togglePopup} className="more">
+                Read more...
+              </p>
+              {isOpen && (
+                <Popup
+                  content={
+                    <>
+                      <b>Info About speaker</b>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                        sint occaecat cupidatat non proident, sunt in culpa qui
+                        officia deserunt mollit anim id est laborum.
+                      </p>
+                    </>
+                  }
+                  handleClose={togglePopup}
+                />
+              )}
+            </div>
+          </div>
+
+          <div className="sub">
+            <div className="speaker-img">
+              <img
+                src={
+                  "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHw%3D&w=1000&q=80"
+                }
+                alt=""
+              />
+            </div>
+            <div className="speaker-content">
+              <h2>Lorem, ipsum.</h2>
+              <p className="speaker-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+                fugit eveniet, ullam deserunt inventore omnis!
+              </p>
+              <p style={{ color: "#f36e2d" }} onClick={togglePopup} className="more">
+                Read more...
+              </p>
+              {isOpen && (
+                <Popup
+                  content={
+                    <>
+                      <b>Info About speaker</b>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                        sint occaecat cupidatat non proident, sunt in culpa qui
+                        officia deserunt mollit anim id est laborum.
+                      </p>
+                    </>
+                  }
+                  handleClose={togglePopup}
+                />
+              )}
+            </div>
+          </div>
+
+
+          <div className="sub">
+            <div className="speaker-img">
+              <img
+                src={
+                  "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHw%3D&w=1000&q=80"
+                }
+                alt=""
+              />
+            </div>
+            <div className="speaker-content">
+              <h2>Lorem, ipsum.</h2>
+              <p className="speaker-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+                fugit eveniet, ullam deserunt inventore omnis!
+              </p>
+              <p style={{ color: "#f36e2d" }} onClick={togglePopup} className="more">
+                Read more...
+              </p>
+              {isOpen && (
+                <Popup
+                  content={
+                    <>
+                      <b>Info About speaker</b>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                        sint occaecat cupidatat non proident, sunt in culpa qui
+                        officia deserunt mollit anim id est laborum.
+                      </p>
+                    </>
+                  }
+                  handleClose={togglePopup}
+                />
+              )}
+            </div>
           </div>
         </div>
-
-        <div className="sub">
-          <div className="speaker-img">
-            <img
-              src={
-                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHw%3D&w=1000&q=80"
-              }
-              alt=""
-            />
-          </div>
-          <div className="speaker-content">
-            <h2>Lorem, ipsum.</h2>
-            <p style={{ margin: "0", padding: "0" }}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
-              fugit eveniet, ullam deserunt inventore omnis!
-            </p>
-            <p style={{ color: "#f36e2d" }} onClick={togglePopup} className="more">
-              Read more...
-            </p> 
-            {isOpen && (
-              <Popup
-                content={
-                  <>
-                    <b>Info About speaker</b>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </p>
-                  </>
-                }
-                handleClose={togglePopup}
-              />
-            )}
-          </div>
-        </div>
-
-
-        <div className="sub">
-          <div className="speaker-img">
-            <img
-              src={
-                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHw%3D&w=1000&q=80"
-              }
-              alt=""
-            />
-          </div>
-          <div className="speaker-content">
-            <h2>Lorem, ipsum.</h2>
-            <p style={{ margin: "0", padding: "0" }}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
-              fugit eveniet, ullam deserunt inventore omnis!
-            </p>
-            <p style={{ color: "#f36e2d" }} onClick={togglePopup} className="more">
-              Read more...
-            </p> 
-            {isOpen && (
-              <Popup
-                content={
-                  <>
-                    <b>Info About speaker</b>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </p>
-                  </>
-                }
-                handleClose={togglePopup}
-              />
-            )}
-          </div>
-        </div>
-
       </div>
     </section>
   );
