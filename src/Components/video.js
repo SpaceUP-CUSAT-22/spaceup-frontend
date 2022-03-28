2f6bf88ee04662ac3423f564c9bdfad9ece83b2c
Rohith C Dinesan <rohithmaddz7@gmail.com>
2022-03-28 17:35:50 +0530

    updated video section

--- src/Components/video.js
+++ src/Components/video.js
@@ -1,20 +1,35 @@
 import videosrc from '../Assets/home-video.mp4';
+import unmute from '../Assets/unmute.svg';
+import mute from '../Assets/mute.svg';
+
 import "./video.css";
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const Video = () => {
-    const videoRef = useRef();
+    const mutebtn = useRef();
+    const [state, setState] = useState(false);
+
+    const sound = () => {
+        mutebtn.current.muted = !mutebtn.current.muted;
+        setState(!state);
+    }
 
     return (
         <div className="video-parent">
             <video
+                ref={mutebtn}
                 src={videosrc}
-                controls
                 muted
                 autoPlay={"autoplay"}
                 preLoad="auto"
                 loop
-            ></video>
+            >
+                
+            </video>
+
+            <div className="btn-vid" onClick={sound}>
+                <img src={(state) ? unmute : mute } alt="unmute" />
+            </div>
         </div>
     );
 };
